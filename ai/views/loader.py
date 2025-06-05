697346db046420f86cd828cd9e46f4a7d5f5ec6f
Maxell Milay <milaymaxell@gmail.com>
2025-06-05 16:24:39 +0800

    added authentication and permission classes

--- ai/views/loader.py
+++ ai/views/loader.py
@@ -3,11 +3,15 @@ from ai.models.document import Document, DocumentChunk
 from ai.serializers.document import DocumentSerializer, DocumentChunkSerializer, SimpleDocumentChunkSerializer
 from ai.lib.loader import DocumentLoader
 from ai.lib.nlp import NLPPreprocessor
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class DocumentView(GenericView):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     allowed_methods = ['create', 'list', 'retrieve', 'destroy']
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
     
     def post_create(self, request, instance):
         print("\nCREATING DOCUMENT CHUNKS\n")
@@ -38,8 +42,12 @@ class DocumentChunkView(GenericView):
     queryset = DocumentChunk.objects.all()
     serializer_class = DocumentChunkSerializer
     allowed_methods = ['retrieve']
-    
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+        
 class SimpleDocumentChunkView(GenericView):
     queryset = DocumentChunk.objects.all()
     serializer_class = SimpleDocumentChunkSerializer
     allowed_methods = ['list']
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
